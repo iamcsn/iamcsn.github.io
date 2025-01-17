782177859032058057f45c72c0abd472a1b9e8fd
iamcsn <3983006050@qq.com>
2025-01-17 21:55:27 +0800

    Site updated: 2025-01-17 21:55:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/14/hello-world/","2025/01/15/第六次/","2025/01/16/2025年.1月.15日-16日笔记/"];function toRandomPost(){
+var posts=["2025/01/14/hello-world/","2025/01/15/第六次/","2025/01/16/2025年.1月.15日-16日笔记/","2025/01/17/1.Spring引入带来了哪些好处？/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
